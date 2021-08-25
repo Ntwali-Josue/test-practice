1ed302e78313ea64d432c8ac165735160bf21036
Ntwali-Josue <kntwalijos@gmail.com>
2021-08-25 20:47:27 +0200

    Test if characters are between 0 and 10

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const stringLength =  require('./index');
 test('test string length', () => {
   expect(stringLength('testing')).toBe(7);
 });
+
+test('test if characters are between 0 & 10', () => {
+  expect(stringLength('instructions')).toBe('Too long')
+})
